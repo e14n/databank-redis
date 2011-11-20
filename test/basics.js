c1e3e6123c54375d1df1edbd20a6db5c30647dc4
Evan Prodromou <evan@status.net>
2011-11-20 11:30:12 -0500

    jshint the basics.js file

--- test/basics.js
+++ test/basics.js
@@ -46,32 +46,32 @@ var main = function(params) {
 
     // Instantiate with factory method
     assert.doesNotThrow(function() {
-        bank = Databank.get(params['driver'], params);
+        bank = Databank.get(params.driver, params);
     }, Error, "Error on get()");
 
     assert.ok(bank);
 
     bank.connect(params, function(err) {
         assert.ifError(err);
-	console.log("Creating object");
+        console.log("Creating object");
         bank.create('test', 1, {'pass': true, 'iters': 42}, function(err, value) {
             assert.ifError(err);
             assert.ok(value);
-            assert.equal(value['pass'], true);
-            assert.equal(value['iters'], 42);
-	    console.log("Fetching object");
+            assert.equal(value.pass, true);
+            assert.equal(value.iters, 42);
+            console.log("Fetching object");
             bank.read('test', 1, function(err, value) {
                 assert.ifError(err);
                 assert.ok(value);
-                assert.equal(value['pass'], true);
-                assert.equal(value['iters'], 42);
-		console.log("Updating object");
+                assert.equal(value.pass, true);
+                assert.equal(value.iters, 42);
+                console.log("Updating object");
                 bank.update('test', 1, {'pass': true, 'iters': 43}, function(err, value) {
                     assert.ifError(err);
                     assert.ok(value);
-                    assert.equal(value['pass'], true);
-                    assert.equal(value['iters'], 43);
-		    console.log("Deleting object");
+                    assert.equal(value.pass, true);
+                    assert.equal(value.iters, 43);
+                    console.log("Deleting object");
                     bank.del('test', 1, function(err) {
                         assert.ifError(err);
                         bank.disconnect(function(err) {
